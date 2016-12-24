daf6bbcb3dd4a3c5e6d1e7c04ce3177444618211
mturner <mhturner@uw.edu>
2016-12-23 16:04:44 -0800

    New LECS figs for +/- surround intensity

--- FigureCode/doLECSAnalysis.m
+++ FigureCode/doLECSAnalysis.m
@@ -61,11 +61,16 @@ function doLECSAnalysis(node,varargin)
             tempResponseMatrix = nan(noPatches,length(centerStims));
             for cc = 1:length(centerStims) %for each stim pair
                 stimPairNode = imageNode.childBySplitValue(centerStims{cc}).childBySplitValue(surroundStims{cc});
+                patchLocations = nan(stimPairNode.children.length,2);
                 for ii = 1:stimPairNode.children.length %for each patch location
                     newResp = getResponseAmplitudeStats(stimPairNode.children(ii).epochList,recType);
                     tempResponseMatrix(ii,cc) = newResp.(metric).mean;
+                    
+                    %mean patch intensity in center & surround
+                    patchLocations(ii,:) = str2num(stimPairNode.children(ii).splitValue);
                 end
             end
+
             %population stats:
             temp = (tempResponseMatrix(:,1) - tempResponseMatrix(:,4)) ./ ...
                 (abs(tempResponseMatrix(:,1)) + abs(tempResponseMatrix(:,4)));
@@ -80,6 +85,31 @@ function doLECSAnalysis(node,varargin)
             NLI.imageSurround(imCt) = nanmean(temp);
 
             if imageNode.custom.get('isExample') %add to example figs
+                ImageID = imageNode.splitValue;
+                sz_micron = imageNode.epochList.firstValue.protocolSettings('annulusOuterDiameter'); %microns
+                patchRes = getNaturalImagePatchFromLocation(patchLocations,ImageID,'imageSize',[sz_micron sz_micron]);
+                sz = size(patchRes.images{1},1); %image pixels
+                radX = sz/2; radY = sz/2;
+                outerDiameter = sz_micron; %microns
+                innerDiameter = imageNode.epochList.firstValue.protocolSettings('annulusInnerDiameter'); % microns
+                [rr, cc] = meshgrid(1:(2*radX),1:(2*radY));
+                apertureMatrix = sqrt((rr-radX).^2 + ...
+                    (cc-radY).^2) < (outerDiameter/2) ./ 6.6;
+                apertureMatrix = apertureMatrix';
+                maskMatrix = sqrt((rr-radX).^2 + ...
+                    (cc-radY).^2) > (innerDiameter/2) ./ 6.6;
+                maskMatrix = maskMatrix';
+                apertureMatrix = min(maskMatrix,apertureMatrix); 
+
+                surroundContrast = zeros(length(patchRes.images),1);
+                for ii = 1:length(patchRes.images)
+                   currentImage = patchRes.images{ii};
+                   surroundIntensity = mean(currentImage(apertureMatrix));
+                   surroundContrast(ii) = (surroundIntensity - patchRes.backgroundIntensity)/ patchRes.backgroundIntensity;
+                end
+                posSurroundIndices = find(surroundContrast > 0);
+                negSurroundIndices = find(surroundContrast < 0);
+                
                 if strcmp(cellInfo.cellType,'ONparasol')
                     tempColor = [0.5 0.5 0.5];
                 elseif strcmp(cellInfo.cellType,'OFFparasol')
@@ -97,23 +127,49 @@ function doLECSAnalysis(node,varargin)
                 
                 colors = pmkmp(3);
                 if strcmp(cellInfo.cellType,'OFFparasol')
-                    % Linearity of center modulated by linear surround
-                    addLineToAxis(tempResponseMatrix(:,1),tempResponseMatrix(:,4),...
-                        [cellInfo.cellType, '_', imageNode.splitValue,'_center'],fig3,'k','none','o')
-                    addLineToAxis(tempResponseMatrix(:,7),tempResponseMatrix(:,6),...
-                        [cellInfo.cellType, '_', imageNode.splitValue,'_linSurround'],fig3,colors(2,:),'none','o')
+                    % Linearity of center modulated by linear surround:
+                    %positive surrounds...
+                    addLineToAxis(tempResponseMatrix(posSurroundIndices,1),tempResponseMatrix(posSurroundIndices,4),...
+                        'centerPos',fig3,'k','none','o')
+                    addLineToAxis(tempResponseMatrix(posSurroundIndices,7),tempResponseMatrix(posSurroundIndices,6),...
+                        'linSurroundPos',fig3,colors(2,:),'none','o')
+                    
+                    %negative surrounds...
+                    addLineToAxis(tempResponseMatrix(negSurroundIndices,1),tempResponseMatrix(negSurroundIndices,4),...
+                        'centerNeg',fig3,'k','none','x')
+                    addLineToAxis(tempResponseMatrix(negSurroundIndices,7),tempResponseMatrix(negSurroundIndices,6),...
+                        'linSurroundNeg',fig3,colors(2,:),'none','x')
+                    
+                    %info & unity...
                     addLineToAxis([downLim upLim],[downLim upLim],...
                         'unity',fig3,'k','--','none')
-                    addLineToAxis(0,0,[cellInfo.cellType,'_',cellInfo.cellID],fig3,'k','none','none')
+                    addLineToAxis(0,0,[cellInfo.cellType(1:3),'_',cellInfo.cellID,'_', imageNode.splitValue],fig3,'k','none','none')
 
-                    % Linearity of center modulated by image surround
-                    addLineToAxis(tempResponseMatrix(:,1),tempResponseMatrix(:,4),...
-                        [cellInfo.cellType, '_', imageNode.splitValue,'_center'],fig4,'k','none','o')
-                    addLineToAxis(tempResponseMatrix(:,3),tempResponseMatrix(:,8),...
-                        [cellInfo.cellType, '_', imageNode.splitValue,'_imSurround'],fig4,colors(3,:),'none','o')
+                    % Linearity of center modulated by image surround:
+                    % positive surrounds...
+                    addLineToAxis(tempResponseMatrix(posSurroundIndices,1),tempResponseMatrix(posSurroundIndices,4),...
+                        'centerPos',fig4,'k','none','o')
+                    addLineToAxis(tempResponseMatrix(posSurroundIndices,3),tempResponseMatrix(posSurroundIndices,8),...
+                        'imSurroundPos',fig4,colors(3,:),'none','o')
+                    
+                    % negative surrounds...
+                    addLineToAxis(tempResponseMatrix(negSurroundIndices,1),tempResponseMatrix(negSurroundIndices,4),...
+                        'centerNeg',fig4,'k','none','x')
+                    addLineToAxis(tempResponseMatrix(negSurroundIndices,3),tempResponseMatrix(negSurroundIndices,8),...
+                        'imSurroundNeg',fig4,colors(3,:),'none','x')
+                    
+                    % lines between center & surround...
+                    for patchPoint = 1:size(tempResponseMatrix,1);
+                        addLineToAxis([tempResponseMatrix(patchPoint,1), tempResponseMatrix(patchPoint,3)],...
+                            [tempResponseMatrix(patchPoint,4), tempResponseMatrix(patchPoint,8)],...
+                        ['connect',num2str(patchPoint)],fig4,'k','-','none')
+                        
+                    end
+                    
+                    %info & unity...
                     addLineToAxis([downLim upLim],[downLim upLim],...
                         'unity',fig4,'k','--','none')
-                    addLineToAxis(0,0,[cellInfo.cellType,'_',cellInfo.cellID],fig4,'k','none','none')
+                    addLineToAxis(0,0,[cellInfo.cellType(1:3),'_',cellInfo.cellID,'_', imageNode.splitValue],fig4,'k','none','none')
                 end
             end
 
